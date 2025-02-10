e3e4f18b2d0441bffff2d56ad0f90e1b884b6087
kolumbian <78987903+kolumbian@users.noreply.github.com>
2025-02-10 10:19:34 +0200

    10.07 11th test

--- commandTest.js
+++ commandTest.js
@@ -301,7 +301,7 @@
         // jsonModification('bkaEnforcementMessageViewModel', []);
         // jsonModification('genericError', []);
         // jsonModification('auxiliaryUi', []);
-        jsonModification('client', []);
+        jsonModification('autoplay', []);
         
   
         creatingFillingStyles(window.location.hostname);
